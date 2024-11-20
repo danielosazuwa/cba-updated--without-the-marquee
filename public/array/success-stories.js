3bd46758dd957043701122b5047a9e97166d6901
unknown <danielosazuwa1@gmail.com>
2024-11-20 21:12:09 +0100

    Home page features implemented

---
+++ public/array/success-stories.js
@@ -0,0 +1,24 @@
+let stories = [
+  {
+    story:
+      "After years in a mid-level banking role, I felt stagnant and sought a change. I enrolled in DeCareer Builders' Business Analysis training, and it was transformative! The hands-on projects taught me essential functions like gathering and analyzing requirements, conducting stakeholder interviews, and creating process flows and mind maps. I learned to use tools like Confluence for documentation....",
+    writer: "Judith C.",
+    role: "Business Analyst",
+  },
+
+  {
+    story:
+      "I was struggling to break into the tech industry when I stumbled upon DeCareer Builders' Data Analysis course, and it turned out to be the breakthrough I needed! The instructors were absolute pros, and the hands-on projects in data visualization, SQL, and Python felt like real-world tasks I'd face on the job. By the time I finished, I not only secured a Data Analyst role at a fast-growing company,.....",
+    writer: "Winifred E.",
+    role: "Data Analyst",
+  },
+
+  {
+    story:
+      "For years I worked in an administrative role, feeling stuck in my career. That all changed when I enrolled in DeCareer Builders Academy’s (DCBA) Scrum Master program. What set DCBA apart was the hands-on work experience. I was placed on a real project, leading a team responsible for optimizing a firm’s website. As the Scrum Master, I organized sprints, facilitated daily stand-ups, and solved communication....",
+    writer: "Folashade D.",
+    role: "Scrum Master",
+  },
+];
+
+module.exports = stories;
